cd2c52a3f0022b9bc64fa75a8638156c8d1bd34d
Zexono <spector6622@gmail.com>
2025-11-29 20:46:18 +0700

    /app func

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/",http.FileServer(http.Dir(".")))
+	h := http.FileServer(http.Dir("."))
+	mux.Handle("/app/",http.StripPrefix("/app", h))
+	mux.HandleFunc("/healthz",app)
 	sv := http.Server{Handler: mux,Addr: ":8080"}
 	sv.ListenAndServe()
+}
+
+func app(w http.ResponseWriter,r *http.Request){
+	w.Header().Set("Content-Type","text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	_,_ = w.Write([]byte("OK"))
 }
